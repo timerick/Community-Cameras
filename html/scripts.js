7bc58f9733c57507fb7dde107e67d838db60aab9
tmr <tmryckman@gmail.com>
2023-10-24 22:19:48 -0700

    tried another way

--- html/scripts.js
+++ html/scripts.js
@@ -41,7 +41,7 @@ function dismissCookiePopup() {
 var videoFrame = document.getElementById('videoFrame');
 
 // Wait for the iframe to be completely loaded
-videoFrame.onload = function() {
+/*videoFrame.onload = function() {
     // Get the content document of the iframe
     var iframeDocument = videoFrame.contentDocument || videoFrame.contentWindow.document;
 
@@ -61,5 +61,18 @@ videoFrame.onload = function() {
             // You can perform actions like redirecting to another page or closing the player.
         }
     }, timeoutInMilliseconds);
-};
+};*/
 
+        // Get the iframe element
+        var iframe = document.getElementById("videoFrame");
+
+        // Wait for the iframe content to load
+        iframe.onload = function() {
+            // Access the video element inside the iframe
+            var video = iframe.contentDocument.getElementById("player_html5_api"); // Replace "your-video-element-id" with the actual ID of your video element
+
+            // Set a timeout to pause the video after 1 minute (60,000 milliseconds)
+            setTimeout(function() {
+                video.pause();
+            }, 10000); // 60000 milliseconds = 1 minute
+        };
